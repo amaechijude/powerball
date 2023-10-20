93f0489ff4b5b255351a918f9f978b0a1f0d3de9
Amaechi <111006036+amaechijude@users.noreply.github.com>
2023-10-20 08:38:48 +0100

    Update powerball.sh

--- powerball.sh
+++ powerball.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "# powerball" >> README.md
 git init
-git add README.md
+git add .
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/amaechijude/powerball.git
